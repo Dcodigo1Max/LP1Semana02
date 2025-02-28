153b50a83d2e27c0363ee2c6c478fe0556610f7d
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-02-28 16:58:40 +0000

    Commit exer6

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //exercicio 6
+            long lg = 2;
+            int i = (int) lg;
+
+            float f = 3.4f;
+            double x = (double) f;
+
+            double c = 4.333;
+            short s =(short) c;
+
+            Console.WriteLine(i);
+            Console.WriteLine(x);
+            Console.WriteLine(s);
+
+            double z = double.PositiveInfinity;
+            double y = double.NegativeInfinity;
+
+            double h = double.PositiveInfinity/double.NegativeInfinity;
+            float h1 = float.NaN;
+
+            Console.WriteLine(z);
+            Console.WriteLine(y);
+            Console.WriteLine(h1);
+
+        }
+    }
+}
