d517489b5a47fa8bcc57b31ba221ab8f95a0a5c7
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-02-28 18:01:51 +0000

    Exerciccio Guess Number

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace GuessTheNumber
 {
@@ -6,7 +7,53 @@ namespace GuessTheNumber
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Random rnd = new Random();
+            int numberToGuess = rnd.Next(31);
+            
+            
+
+           
+            while(true)
+            {
+                Console.WriteLine("Escreve um numero inteiro entre 0 e 30:");
+                string str1 = Console.ReadLine();
+                
+
+                //if (str1 < 0)
+                
+                
+
+                if (Convert.ToInt32(str1)<0 || Convert.ToInt32(str1)>30)
+                {
+                    Console.WriteLine("O numero inserido é  menor que 0 ou maior que 30");
+                    
+                }
+
+
+
+
+                else if (Convert.ToInt32(str1) < numberToGuess || Convert.ToInt32(str1) > numberToGuess)
+                {
+                    Console.WriteLine("O numero inserido é  maior ou menor que o numero aleatorio");
+                }
+
+
+
+
+                else if(Convert.ToInt32(str1) == numberToGuess)
+                {
+                    Console.WriteLine("O numero inserido é  igual ao numero aleatorio");
+
+                }
+
+                break;
+
+
+            }
+
+
+
+
         }
     }
 }
