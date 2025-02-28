051362ffc18c656d750ba492aec3377ed382928c
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-02-28 17:19:54 +0000

    ChatGPTOne done

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+           Exercicio 10
+           
+            string questions = Console.ReadLine();
+
+            do 
+                if (questions == "What is pie in Math?")
+                {
+                    Console.WriteLine("3.14");
+                    break;
+                   
+                }
+
+                else if (questions == "What are you?")
+                { 
+                    Console.Write("Im a computer");
+                   break;
+                }
+
+                else if (questions == "What is todays date?")
+                { 
+                    Console.Write("Check a calendar");
+                    break;
+                }
+
+                else if (questions == "What are you?")
+                { 
+                    Console.Write("Im a computer");
+                   break;
+                }
+
+                else
+
+                    Console.WriteLine($"Sorry didn't understand the question");               
+            
+                       
+            while(questions != "EXIT");
+        }
+    }
+}
