5bebd553e72a9cda5d26263cf1764920524e4895
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-02-28 16:55:12 +0000

    Cilindro feito

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,34 @@ namespace Cilindro
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            //Exercicio 5
+            
+            //Pedir altura
+            Console.WriteLine("Escreve a altura do cilindro:");
+            string str1 = Console.ReadLine();
+            int a = int.Parse(str1);
+            Convert.ToDouble(a);
+
+            //Pedir raio
+            Console.WriteLine("Escreve o raio do cilindro:");
+            string str2 = Console.ReadLine();
+            int r = int.Parse(str2);
+            Convert.ToDouble(r);
+            
+            
+            //Calculo Volume
+            Console.WriteLine("Calculo V:");
+            double v = Math.PI * Math.Pow(r*r, a);
+            
+            Console.WriteLine(v);
+            
+
+            //Areasuperficie Sa
+            Console.WriteLine("Calculo Sa:");
+            double Sa = 2 *Math.PI * r * (r * a);
+            Console.WriteLine(Sa);
+
+
         }
     }
 }
